e35097fe7d64f7055213304e9a68d51845131437
shfischer <32540427+shfischer@users.noreply.github.com>
2018-11-28 13:32:30 +0000

    Preparation for moving MSE into new a4a standard framework.

--- OM.R
+++ OM.R
@@ -7,13 +7,17 @@
 library(FLfse)
 library(ggplotFL)
 library(FLAssess)
+# library(mse)
+### load files from package mse for easier debugging
+devtools::load_all("../mse/")
 library(FLash)
 library(tidyr)
 library(dplyr)
 
 ### source the scripts from functions folder
-invisible(lapply(list.files(path = "functions/", pattern = "*.R$", 
-                            full.names = TRUE), source))
+# invisible(lapply(list.files(path = "functions/", pattern = "*.R$", 
+#                             full.names = TRUE), source))
+source("a4a_mse_WKNSMSE_funs.R")
 
 dir.create(path = "input/cod4", recursive = TRUE)
 dir.create(path = "output/runs", recursive = TRUE)
@@ -22,14 +26,12 @@ dir.create(path = "output/runs", recursive = TRUE)
 ### simulation specifications ####
 ### ------------------------------------------------------------------------ ###
 
-### number of iterations
+### number of iterations/replicates
 n <- 100
 ### number of years
-n_years <- 30
-
+n_years <- 20
 ### last data year
 yr_data <- 2018
-
 
 ### ------------------------------------------------------------------------ ###
 ### fit SAM ####
@@ -79,10 +81,11 @@ landings(cod4_stk2)[, ac(yrs)] <- computeLandings(cod4_stk2)[, ac(yrs)]
 discards(cod4_stk2)[, ac(yrs)] <- computeDiscards(cod4_stk2)[, ac(yrs)]
 
 ### fit SAM to "corrected" catches
+cod4_conf_sam_no_mult <- cod4_conf_sam[!names(cod4_conf_sam) %in% 
+                                         c("noScaledYears", "keyScaledYears",
+                                           "keyParScaledYA")]
 fit2 <- FLR_SAM(stk = cod4_stk2, idx = cod4_idx, 
-                conf = cod4_conf_sam[!names(cod4_conf_sam) %in% 
-                                       c("noScaledYears", "keyScaledYears",
-                                         "keyParScaledYA")])
+                conf = cod4_conf_sam_no_mult)
 ### compare results
 summary(fit2) / summary(fit)
 ### estimates and log likelihood identical, only bounds smaller
@@ -98,8 +101,8 @@ stk <- SAM2FLStock(object = fit, stk = cod4_stk2)
 summary(stk)
 
 ### set units
-units(stk)[1:17] <- as.list(c(rep(c("tonnes", "thousands", "kg"), 4),
-                              "NA", "NA", "f", "NA", "NA"))
+units(stk)[1:17] <- as.list(c(rep(c("t", "1000", "kg"), 4),
+                              "", "", "f", "", ""))
 plot(stk)
 
 ### save for later comparison
@@ -112,7 +115,7 @@ stk_orig <- stk
 
 
 ### add iteration dimension
-stk <- propagate(stk, n)
+stk <- FLCore::propagate(stk, n)
 dim(stk)
 
 ### add uncertainty estimated by SAM as iterations
@@ -132,98 +135,98 @@ plot(stk, probs = c(0.05, 0.25, 0.5, 0.75, 0.95))
 ### ------------------------------------------------------------------------ ###
 ### check MCMC approach ####
 ### ------------------------------------------------------------------------ ###
-library(tmbstan) # cran package
-
-### create template stock for storing results
-MCMC_iter <- n
-MCMC_warmup <- 1000
-stk_MCMC <- propagate(stk, MCMC_iter)
-
-### run MCMC
-system.time(mcmc <- tmbstan(fit$obj, chains = 1, iter = MCMC_warmup + MCMC_iter, 
-                            warmup = MCMC_warmup, 
-                            seed = 1, control = list(max_treedepth = 15)))
-### extract 
-mc <- extract(mcmc, inc_warmup = FALSE, permuted = FALSE)
-
-table(gsub(x = dimnames(mc)$parameters, pattern = "\\[[0-9]{1,}\\]", 
-           replacement = ""))
-# itrans_rho         logF      logFpar         logN     logScale logSdLogFsta 
-#          1          336            9          336           13            2 
-# logSdLogN  logSdLogObs         lp__ 
-#         2            7            1 
-### gives N and F @age
-### scale for 
-
-
-### find positions for results in MCMC
-nms <- dimnames(mc)$parameters
-F_pos <- grep(x = nms, pattern = "logF\\[[0-9]{1,3}\\]$")
-N_pos <- grep(x = nms, pattern = "logN\\[[0-9]{1,3}\\]$")
-factor_pos <- grep(x = nms, pattern = "logScale\\[[0-9]{1,2}\\]$")
-
-### in the MCMC results age and years are mixed within the same row,
-### each row represents one iteration
-### this needs to be reformatted to be useful...
-
-### fishing mortality:
-harvest(stk_MCMC)[] <- aperm(array(data = c(exp(mc[,, F_pos])),
-                                   dim = dim(harvest(stk_MCMC))[c(6, 1:5)]),
-                             perm = c(2:6, 1))
-
-### stock numbers at age
-stock.n(stk_MCMC)[] <- aperm(array(data = c(exp(mc[,, N_pos])),
-                                   dim = dim(stock.n(stk_MCMC))[c(6, 1:5)]),
-                             perm = c(2:6, 1))
-stock(stk_MCMC) <- computeStock(stk_MCMC)
-
-### catch factor
-### get ages and years
-ages <- fit$conf$minAge:fit$conf$maxAge
-yrs <- fit$conf$keyScaledYears
-### get catch factors
-catch_factor <- lapply(split(exp(mc[,, factor_pos]), seq(MCMC_iter)), 
-                       function(x) {
-  x[t(fit$conf$keyParScaledYA + 1)]
-})
-catch_factor <- unlist(catch_factor)
-### coerce into FLQuant
-catch_factor <- FLQuant(catch_factor,
-                      dimnames = dimnames(catch.n(stk_MCMC[ac(ages), ac(yrs)])))
-### multiply catch numbers
-catch.n(stk_MCMC)[ac(ages), ac(yrs)] <- catch_factor * 
-  catch.n(stk_MCMC)[ac(ages), ac(yrs)]
-### split into landings and discards, based on landing fraction
-lfrac <- propagate((landings.n(stk)[ac(ages), ac(yrs)] / 
-                      catch.n(stk)[ac(ages), ac(yrs)]), MCMC_iter)
-landings.n(stk_MCMC)[ac(ages), ac(yrs)] <- catch.n(stk_MCMC)[ac(ages), ac(yrs)] *
-  lfrac
-discards.n(stk_MCMC)[ac(ages), ac(yrs)] <- catch.n(stk_MCMC)[ac(ages), ac(yrs)] *
-  (1 - lfrac)
-### update stock
-catch(stk_MCMC)[, ac(yrs)] <- computeCatch(stk_MCMC)[, ac(yrs)]
-landings(stk_MCMC)[, ac(yrs)] <- computeLandings(stk_MCMC)[, ac(yrs)]
-discards(stk_MCMC)[, ac(yrs)] <- computeDiscards(stk_MCMC)[, ac(yrs)]
-
-### plot MCMC stock
-plot(stk_MCMC)
-### compare with original SAM fit
-plot(FLStocks(MCMC = stk_MCMC, original = stk_orig))
-### compare with first uncertainty approach
-plot(FLStocks(MCMC = stk_MCMC, original = stk_orig, Cov = stk))
+# library(tmbstan) # cran package
+# 
+# ### create template stock for storing results
+# MCMC_iter <- n
+# MCMC_warmup <- 1000
+# stk_MCMC <- propagate(stk, MCMC_iter)
+# 
+# ### run MCMC
+# system.time(mcmc <- tmbstan(fit$obj, chains = 1, iter = MCMC_warmup + MCMC_iter, 
+#                             warmup = MCMC_warmup, 
+#                             seed = 1, control = list(max_treedepth = 15)))
+# ### extract 
+# mc <- extract(mcmc, inc_warmup = FALSE, permuted = FALSE)
+# 
+# table(gsub(x = dimnames(mc)$parameters, pattern = "\\[[0-9]{1,}\\]", 
+#            replacement = ""))
+# # itrans_rho         logF      logFpar         logN     logScale logSdLogFsta 
+# #          1          336            9          336           13            2 
+# # logSdLogN  logSdLogObs         lp__ 
+# #         2            7            1 
+# ### gives N and F @age
+# ### scale for 
+# 
+# 
+# ### find positions for results in MCMC
+# nms <- dimnames(mc)$parameters
+# F_pos <- grep(x = nms, pattern = "logF\\[[0-9]{1,3}\\]$")
+# N_pos <- grep(x = nms, pattern = "logN\\[[0-9]{1,3}\\]$")
+# factor_pos <- grep(x = nms, pattern = "logScale\\[[0-9]{1,2}\\]$")
+# 
+# ### in the MCMC results age and years are mixed within the same row,
+# ### each row represents one iteration
+# ### this needs to be reformatted to be useful...
+# 
+# ### fishing mortality:
+# harvest(stk_MCMC)[] <- aperm(array(data = c(exp(mc[,, F_pos])),
+#                                    dim = dim(harvest(stk_MCMC))[c(6, 1:5)]),
+#                              perm = c(2:6, 1))
+# 
+# ### stock numbers at age
+# stock.n(stk_MCMC)[] <- aperm(array(data = c(exp(mc[,, N_pos])),
+#                                    dim = dim(stock.n(stk_MCMC))[c(6, 1:5)]),
+#                              perm = c(2:6, 1))
+# stock(stk_MCMC) <- computeStock(stk_MCMC)
+# 
+# ### catch factor
+# ### get ages and years
+# ages <- fit$conf$minAge:fit$conf$maxAge
+# yrs <- fit$conf$keyScaledYears
+# ### get catch factors
+# catch_factor <- lapply(split(exp(mc[,, factor_pos]), seq(MCMC_iter)), 
+#                        function(x) {
+#   x[t(fit$conf$keyParScaledYA + 1)]
+# })
+# catch_factor <- unlist(catch_factor)
+# ### coerce into FLQuant
+# catch_factor <- FLQuant(catch_factor,
+#                       dimnames = dimnames(catch.n(stk_MCMC[ac(ages), ac(yrs)])))
+# ### multiply catch numbers
+# catch.n(stk_MCMC)[ac(ages), ac(yrs)] <- catch_factor * 
+#   catch.n(stk_MCMC)[ac(ages), ac(yrs)]
+# ### split into landings and discards, based on landing fraction
+# lfrac <- propagate((landings.n(stk)[ac(ages), ac(yrs)] / 
+#                       catch.n(stk)[ac(ages), ac(yrs)]), MCMC_iter)
+# landings.n(stk_MCMC)[ac(ages), ac(yrs)] <- catch.n(stk_MCMC)[ac(ages), ac(yrs)] *
+#   lfrac
+# discards.n(stk_MCMC)[ac(ages), ac(yrs)] <- catch.n(stk_MCMC)[ac(ages), ac(yrs)] *
+#   (1 - lfrac)
+# ### update stock
+# catch(stk_MCMC)[, ac(yrs)] <- computeCatch(stk_MCMC)[, ac(yrs)]
+# landings(stk_MCMC)[, ac(yrs)] <- computeLandings(stk_MCMC)[, ac(yrs)]
+# discards(stk_MCMC)[, ac(yrs)] <- computeDiscards(stk_MCMC)[, ac(yrs)]
+# 
+# ### plot MCMC stock
+# plot(stk_MCMC)
+# ### compare with original SAM fit
+# plot(FLStocks(MCMC = stk_MCMC, original = stk_orig))
+# ### compare with first uncertainty approach
+# plot(FLStocks(MCMC = stk_MCMC, original = stk_orig, Cov = stk))
 
 ### ------------------------------------------------------------------------ ###
 ### try SAM internal "simstudy" ####
 ### ------------------------------------------------------------------------ ###
 ### "Simulate data from fitted model and re-estimate from each run"
 
-set.seed(0)
-system.time(fits <- simstudy(fit = fit, nsim = n))
-class(fits) <- "sam_list"
-
-stk_sim <- SAM2FLStock(object = fits, stk = cod4_stk2)
-plot(FLStocks(Cov = stk, simstudy = stk_sim,
-              original = stk_orig))
+# set.seed(0)
+# system.time(fits <- simstudy(fit = fit, nsim = n))
+# class(fits) <- "sam_list"
+# 
+# stk_sim <- SAM2FLStock(object = fits, stk = cod4_stk2)
+# plot(FLStocks(Cov = stk, simstudy = stk_sim,
+#               original = stk_orig))
 
 ### ------------------------------------------------------------------------ ###
 ### extend stock for MSE simulation ####
@@ -232,9 +235,9 @@ plot(FLStocks(Cov = stk, simstudy = stk_sim,
 ### special case for NS cod
 ### catch & catch weights until 2017,
 ### maturity, stock weights, stock & F etc until 2018
-stk_stf2017 <- stf(window(stk, end = 2017), n_years + 1)
+# stk_stf2017 <- stf(window(stk, end = 2017), n_years + 1)
 stk_stf2018 <- stf(window(stk, end = 2018), n_years)
-### 
+### use all available data
 stk_stf <- stk_stf2018
 ### maturity data 2018 is already average of previous years
 ### use this in projection
@@ -257,7 +260,7 @@ plot(stk_stf)
 # Truncated stock objects for defining different recruitment regimes
 # Note EQSIM would end with the data rather than intermediate year 
 #stk_trun<-window(stk, start=1987, end=2017)
-stk_trun2<-window(stk, start=1997, end=2018)
+stk_trun2 <- window(stk, start = 1997, end = 2018)
 
 ### create stock recruitment model: segmented regression (hockey-stick)
 sr <- as.FLSR(stk_trun2, model = "segreg")
@@ -291,6 +294,9 @@ sr_res <- exp(sr_res)
 
 plot(sr_res)
 
+### store residuals in model object
+residuals(sr) <- sr_res
+
 ### ------------------------------------------------------------------------ ###
 ### stf for 2018: assume catch advice is taken ####
 ### ------------------------------------------------------------------------ ###
@@ -323,48 +329,29 @@ idx <- window(idx, end = yr_data + n_years)
 ### add iterations
 idx <- lapply(idx, propagate, n)
 
-### get catchability for surveys and add random noise
-idx <- lapply(seq_along(idx), function(x) {
+### insert catchability
+for (idx_i in seq_along(idx)) {
   
-  ### select index
-  idx_i <- idx[[x]]
+  ### set catchability for projection
+  index.q(idx[[idx_i]])[] <- uncertainty$survey_catchability[[idx_i]]
   
-  ### age range
-  ages_i <- range(idx_i)[["min"]]:range(idx_i)[["max"]]
-  ### year range
-  years_i <- range(idx_i)[["minyear"]]:range(idx_i)[["maxyear"]]
-  ### survey timing
-  time_i <- mean(range(idx_i)[c("startf", "endf")])
-  
-  ### get catchability from SAM
-  q <- uncertainty$survey_catchability[[x]]
-  
-  ### get survey sd & adapt dimensions
-  idx_sd <- window(uncertainty$survey_sd[[x]], start = min(years_i), 
-                   end = max(years_i))
-  idx_sd[] <- uncertainty$survey_sd[[x]]
-  
-  ### create random noise, based on survey sd at age
-  set.seed(2)
-  noise <- idx_sd %=% 0
-  noise[] <- stats::rnorm(n = length(idx_sd), mean = 0, sd = idx_sd)
-  noise <- exp(noise)
-  
-  ### remove noise from first iteration
-  noise[,,,,, 1] <- 1
-  
-  ### add noise to catchability
-  index.q(idx_i)[] <- q
-  index.q(idx_i) <- index.q(idx_i) * noise
-  
-  return(idx_i)
-  
-})
-names(idx) <- names(cod4_idx)
-idx <- FLIndices(idx)
-
-### initialize index values
+}
+### calculate index values
 idx <- calc_survey(stk = stk_fwd, idx = idx)
+
+### create deviances for indices
+### first, get template
+idx_dev <- lapply(idx, index)
+### create random noise based on sd
+set.seed(2)
+for (idx_i in seq_along(idx_dev)) {
+  ### insert sd
+  idx_dev[[idx_i]][] <- uncertainty$survey_sd[[idx_i]]
+  ### noise
+  idx_dev[[idx_i]][] <- stats::rnorm(n = length(idx_dev[[idx_i]]),
+                                   mean = 0, sd = idx_dev[[idx_i]])
+  idx_dev[[idx_i]] <- exp(idx_dev[[idx_i]])
+}
 
 
 ### compare to original survey(s)
@@ -418,29 +405,8 @@ plot(catch_res)
 # 
 
 ### ------------------------------------------------------------------------ ###
-### remove fishy first iteration ####
-### ------------------------------------------------------------------------ ###
-### lengthy code in order to get iteration dimnames 1:n and not 2:n+1
-
-# stk_fwd2 <- stk_fwd[,,,,, 1:n]
-# stk_fwd2[] <- stk_fwd[,,,,, 2:(n + 1)]
-# 
-# sr2 <- FLCore::iter(sr, 1:n)
-# sr2[] <- FLCore::iter(sr, 2:(n + 1))
-# 
-# sr_res2 <- FLCore::iter(sr_res, 1:n)
-# sr_res2[] <- FLCore::iter(sr_res, 2:(n + 1))
-# 
-# idx2 <- idx
-# idx2[[1]] <- FLCore::iter(idx[[1]], 1:n)
-# idx2[[1]][] <- FLCore::iter(idx[[1]], 2:(n + 1))
-# idx2[[2]] <- FLCore::iter(idx[[2]], 1:n)
-# idx2[[2]][] <- FLCore::iter(idx[[2]], 2:(n + 1))
-
-### ------------------------------------------------------------------------ ###
 ### save OM ####
 ### ------------------------------------------------------------------------ ###
-
 
 ### stock
 saveRDS(stk_fwd, file = "input/cod4/stk.rds")
@@ -452,5 +418,75 @@ saveRDS(sr_res, file = "input/cod4/sr_res.rds")
 saveRDS(idx, file = "input/cod4/idx.rds")
 ### catch noise
 saveRDS(catch_res, file = "input/cod4/catch_res.rds")
+
+### ------------------------------------------------------------------------ ###
+### prepare objects for new a4a standard mse package ####
+### ------------------------------------------------------------------------ ###
+### https://github.com/flr/mse
+
+### some arguments (passed to mp())
+genArgs <- list(fy = yr_data + n_years - 1, ### final simulation year
+                y0 = yr_data, ### first simulation year
+                iy = yr_data,
+                nsqy = 3 ### not used, but has to provided
+)
+
+### operating model
+om <- FLom(stock = stk_fwd, ### stock 
+           sr = sr ### stock recruitment and precompiled residuals
+           )
+
+### observation (error) model
+oem <- FLoem(method = oem_WKNSMSE,
+             observations = list(stk = stk_fwd, idx = idx), 
+             deviances = list(stk = FLQuants(catch.dev = catch_res), 
+                              idx = idx_dev),
+             args = list(idx_timing = c(0, -1),
+                         catch_timing = -1,
+                         use_catch_residuals = TRUE, 
+                         use_idx_residuals = TRUE))
+
+### default management
+ctrl_obj <- mpCtrl(list(
+  ctrl.est = mseCtrl(method = SAM_wrapper,
+                     args = list(forecast = TRUE, 
+                                 fwd_trgt = "fsq",
+                                 fwd_yrs = 1, 
+                                 fwd_yrs_average = -3:0, 
+                                 fwd_yrs_rec_start = 1998, 
+                                 fwd_yrs_sel = -3:-1,
+                                 fwd_yrs_lf_remove = -2:-1,
+                                 fwd_splitLD = TRUE,
+                                 newtonsteps = 0,
+                                 rel.tol = 0.001,
+                                 conf = cod4_conf_sam_no_mult,
+                                 parallel = TRUE ### TESTING ONLY
+                                 )),
+  ctrl.phcr = mseCtrl(method = phcr_WKNSMSE,
+                      args = list(Btrigger = 150000,
+                                  Ftrgt = 0.31)),
+  #ctrl.hcr = mseCtrl(method = fixedF.hcr, args = list(ftrg = 0.3))
+  ctrl.hcr = mseCtrl(method = hcr_WKNSME, args = list(option = "A")),
+  ctrl.is = mseCtrl(method = is_WKNSMSE, 
+                    args = list(fwd_trgt = c("fsq", "hcr"),
+                                fwd_yrs = 2,
+                                fwd_yrs_average = -3:0,
+                                fwd_yrs_rec_start = 1998,
+                                fwd_yrs_sel = -3:-1,
+                                fwd_yrs_lf_remove = -2:-1,
+                                fwd_splitLD = TRUE,
+                                TAC_constraint = TRUE,
+                                lower = -Inf, upper = Inf,
+                                Btrigger_cond = FALSE)),
+  ctrl.tm = NULL
+))
+
+
+### try running mse
+# debugonce(mp)
+res1 <- mp(om = om, 
+           oem = oem, 
+           ctrl.mp = ctrl_obj, 
+           genArgs = genArgs)
 
 # knitr::spin(hair = "OM.R", format = "Rmd", precious = TRUE, comment = c('^### ------------------------------------------------------------------------ ###$', '^### ------------------------------------------------------------------------ ###$'))
