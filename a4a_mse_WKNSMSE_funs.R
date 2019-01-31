cbc65d10613d4d0574d4c77b303666d0f1bd0536
Nicola Walker (Cefas) <nicola.walker@cefas.co.uk>
2019-01-31 16:34:07 +0000

    capture survey covariance output

--- a4a_mse_WKNSMSE_funs.R
+++ a4a_mse_WKNSMSE_funs.R
@@ -1029,14 +1029,14 @@ SAM_uncertainty <- function(fit, n = 1000, print_screen = FALSE) {
     
   })
   
-  survey_cov <- foreach(iter_i = 1:n) %do% {
+  . <- capture.output(survey_cov <- foreach(iter_i = 1:n) %do% {
     
     fit$obj$fn(sim.states[iter_i, 1:length(sds$par.fixed)])
     cov <- fit$obj$report()$obsCov
     
     return(cov[idx_surveys])
     
-  }
+  })
   
   ### ---------------------------------------------------------------------- ###
   ### stock.n uncertainty ####
